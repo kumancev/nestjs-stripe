d7f98e4652df0cefea9f80a655780a4bef45e9df
Alex Kumancev <kumancev1@gmail.com>
2023-08-20 21:50:15 +0300

    connect postgres & typeorm & user CRUD

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  app.enableCors({
+    origin: configService.get('FRONTEND_URL'),
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/users/users.service.ts
@@ -0,0 +1,48 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import User from 'src/models/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async create(user: CreateUserDto) {
+    const newUser = await this.usersRepository.create(user);
+    await this.usersRepository.save(newUser);
+    return newUser;
+  }
+
+  async findAll() {
+    return await this.usersRepository.find();
+  }
+
+  async findById(id: number) {
+    const user = await this.usersRepository.findOne({ where: { id } });
+    if (user) {
+      return user;
+    }
+    throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+  }
+
+  async update(id: number, userName: UpdateUserDto) {
+    await this.usersRepository.update(id, userName);
+    const updatedUser = await this.usersRepository.findOne({ where: { id } });
+    if (updatedUser) {
+      return updatedUser;
+    }
+    throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+  }
+
+  async remove(id: number) {
+    const deleteResponse = await this.usersRepository.delete(id);
+    if (!deleteResponse.affected) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+  }
+}
